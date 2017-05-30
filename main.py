3742e61bddc22a8f94e9730d3406c0c60f80c0e4
Edo Freriks <edofre@gmail.com>
2017-05-30 22:51:44 +0200

    Put the timer in a class

--- main.py
+++ main.py
@@ -1,34 +1,58 @@
+__author__ = 'Edo Freriks'
+
 '''
- Countdown timer for an escape room type game
+
 '''
+
 import Tkinter as tk
 import time
 
-def count_down():
-    # start with 60 minutes --> 360 seconds
-    for t in range(3600, -1, -1):
-        # format as 2 digit integers, fills with zero to the left
-        # divmod() gives minutes, seconds
-        sf = "{:02d}:{:02d}".format(*divmod(t, 60))
-        # print(sf)  # test
-        time_str.set(sf)
-        root.update()
-        # delay one second
-        time.sleep(1)
+
+class Testing(tk.Tk):
+    def __init__(self, parent):
+        tk.Tk.__init__(self, parent)
+        self.parent = parent
+        self.initialize()
+
+    def initialize(self):
+        self.time_str = tk.StringVar()
+        # create the time display label, give it a large font
+        # label auto-adjusts to the font
+        label_font = ('helvetica', 40)
+        tk.Label(self, textvariable=self.time_str, font=label_font, bg='white',
+                 fg='blue', relief='raised', bd=3).pack(fill='x', padx=5, pady=5)
+
+        # Create the start timer button, remove it after it's clicked
+        tk.Button(self, text='Count Start', command=self.count_down).pack()
+
+        self.labelVariable = tk.StringVar()
+        tk.Label(self, textvariable=self.labelVariable).pack()
+        self.labelVariable.set("Enter the code to stop the timer. \n If the code is not correct you will lose time!")
+
+        # Add a button that enables the user to enter the code
+        tk.Button(self, text='Enter code', command=self.on_click).pack()
+        self.update()
+
+    def count_down(self):
+        # start with 60 minutes --> 360 seconds
+        for t in range(3600, -1, -1):
+            # format as 2 digit integers, fills with zero to the left
+            # divmod() gives minutes, seconds
+            sf = "{:02d}:{:02d}".format(*divmod(t, 60))
+            # print(sf)  # test
+            self.time_str.set(sf)
+            self.update()
+            # delay one second
+            time.sleep(1)
+
+    def on_click(self):
+        self.labelVariable.set(self.entryVariable.get() + " (You clicked the button)")
+        self.passwordVariable.set(self.id_generator())
+        self.entry.focus_set()
+        self.entry.selection_range(0, tk.END)
 
 
-# create root/main window
-root = tk.Tk()
-time_str = tk.StringVar()
-# create the time display label, give it a large font
-# label auto-adjusts to the font
-label_font = ('helvetica', 40)
-tk.Label(root, textvariable=time_str, font=label_font, bg='white',
-         fg='blue', relief='raised', bd=3).pack(fill='x', padx=5, pady=5)
-# create start and stop buttons
-# pack() positions the buttons below the label
-tk.Button(root, text='Count Start', command=count_down).pack()
-# stop simply exits root window
-tk.Button(root, text='Count Stop', command=root.destroy).pack()
-# start the GUI event loop
-root.mainloop()
+if __name__ == "__main__":
+    app = Testing(None)
+    app.title(__author__)
+    app.mainloop()
